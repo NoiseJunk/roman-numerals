8f56240056d0c54b9813e68cd60a644082a84c62
Simon Middleton <simon.middleton@verseone.com>
2025-02-10 19:39:51 +0000

    changed the results text

--- src/components/RomanNumeral.js
+++ src/components/RomanNumeral.js
@@ -2,9 +2,11 @@ import React from 'react';
 import styles from './RomanNumeral.module.css';
 
 function RomanNumeral({ result }) {
+    const displayText = result === '' ? <span>Result will appear here</span> : result;
+    
     return (
         <div className={styles.romannumeral}>
-            <p className={styles.result}>{result}</p>
+            <p className={styles.result}>{displayText}</p>
         </div>
     );
 }
